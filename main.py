6dd7885b007e7e9b3a0821d818dc4e4ba5f8b970
Jiang Wenjian <jwenjian.me@gmail.com>
2019-10-09 20:39:11 +0800

    Update main.py
    get latest comment not oldest

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def bundle_cover_image_section() -> str:
     comments = cover_issues[0].get_comments()
     if not comments:
         return ''
-    c = comments[0]
+    c = comments[:-1]
     img_md = None
     img_desc = ''
     if '---' in c.body:
